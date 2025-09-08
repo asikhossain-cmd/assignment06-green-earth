bfef1088bdb9398283a0b5c3842b50e828dfb0bd
Asik <asikhossain592445@gmail.com>
2025-09-08 17:36:18 +0600

    added all product dynamically and added partially product show and view all button functionality

--- scripts/script.js
+++ scripts/script.js
@@ -22,4 +22,83 @@ const displayCategory = (categories) => {
         getCategorySection.appendChild(createNewElement)
     })
 }
+
+
+// Fetch and Display All Trees
+const getAllPlant = () => {
+    const url = 'https://openapi.programming-hero.com/api/plants'
+    fetch(url)
+    .then((response) => response.json())
+    .then(resData => {
+        displayAllTrees(resData.plants)
+        
+        }
+    )
+};
+
+const viewAllPlants = () => {
+    const url = 'https://openapi.programming-hero.com/api/plants'
+    fetch(url)
+    .then((response) => response.json())
+    .then(resData => {viewAllFunc(resData.plants)})
+};
+
+// Display All Plants
+const displayAllTrees = (plants) => {
+    for(const plant of plants.slice(1,7)) {
+        // Get the display trees section
+        const getPlantsContainer = document.getElementById('tree-card-container');
+
+
+        const createNewPlatsDiv = document.createElement('div');
+        createNewPlatsDiv.innerHTML = `
+                <div class="tree-card max-w-80 h-full p-4 bg-white rounded-xl flex flex-col justify-between">
+                    <img src="${plant.image}" alt="Picture" class="h-50 w-[100vw] rounded-xl">
+                    <h2 class="text-lg font-semibold mt-3">${plant.name}</h2>
+                    <p class="text-sm font-normal text-gray-700 text-justify mt-2">${plant.description}</p>
+                    <div class="flex justify-between items-center">
+                        <p class="px-4 py-1 rounded-2xl text-[#15803D] bg-green-200 mt-2">Fruit Tree</p>
+                        <p class="font-semibold">৳${plant.price}</p>
+                    </div>
+                    <button class="w-full bg-[#15803D] p-2 rounded-3xl text-white mt-3 cursor-pointer">Add to Cart</button>
+                </div>
+        `;
+
+        getPlantsContainer.appendChild(createNewPlatsDiv)
+
+    }
+    // Get the All trees button
+    const allTreesBtn = document.getElementById('all-trees-btn')
+    allTreesBtn.style.backgroundColor = '#15803D'
+    allTreesBtn.style.color = 'white'
+}
+
+// View All Plant
+    const viewAllFunc = (plants) => {
+        const getPlantsContainer = document.getElementById('tree-card-container');
+        getPlantsContainer.innerHTML = '';
+        for(const plant of plants) {
+        // Get the display trees section
+
+        const createNewPlatsDiv = document.createElement('div');
+        createNewPlatsDiv.innerHTML = `
+                <div class="tree-card max-w-80 h-full p-4 bg-white rounded-xl flex flex-col justify-between">
+                    <img src="${plant.image}" alt="Picture" class="h-50 w-[100vw] rounded-xl">
+                    <h2 class="text-lg font-semibold mt-3">${plant.name}</h2>
+                    <p class="text-sm font-normal text-gray-700 text-justify mt-2">${plant.description}</p>
+                    <div class="flex justify-between items-center">
+                        <p class="px-4 py-1 rounded-2xl text-[#15803D] bg-green-200 mt-2">${plant.category}</p>
+                        <p class="font-semibold">৳${plant.price}</p>
+                    </div>
+                    <button class="w-full bg-[#15803D] p-2 rounded-3xl text-white mt-3 cursor-pointer">Add to Cart</button>
+                </div>
+        `;
+            
+        getPlantsContainer.appendChild(createNewPlatsDiv)
+
+    }
+    const getViewBtn = document.getElementById('view-all-btn');
+    getViewBtn.style.display = 'none';
+}
+getAllPlant()
 getPlantCategory()
