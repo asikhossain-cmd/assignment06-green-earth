e26b5f4ee82a068d11fe0c24cde70a694e35f37f
Asik Hossain <asikhossain592445@gmail.com>
2025-09-08 22:12:46 +0600

    integrated categorywise product browsing

--- scripts/script.js
+++ scripts/script.js
@@ -16,7 +16,7 @@ const displayCategory = (categories) => {
         const createNewElement = document.createElement('div')
         createNewElement.innerHTML = `
             <div id="category-name-section" class="category-name flex flex-col gap-1">
-                <p class="text-lg font-medium w-full text-left py-2 px-2 hover:text-white transition-all rounded-md hover:bg-[#15803D] cursor-pointer">${categoryName}</p>
+                <p onclick = "getPlantsByCategory(${category.id})" class="categories-name-btn text-lg font-medium w-full text-left py-2 px-2 hover:text-white transition-all rounded-md hover:bg-[#15803D] cursor-pointer">${categoryName}</p>
             </div>
         `;
         getCategorySection.appendChild(createNewElement)
@@ -100,5 +100,44 @@ const displayAllTrees = (plants) => {
     const getViewBtn = document.getElementById('view-all-btn');
     getViewBtn.style.display = 'none';
 }
+
+
+// Let's Arrange items by category
+
+// Fetch product by category
+const getPlantsByCategory = (id) => {
+    const url = `https://openapi.programming-hero.com/api/category/${id}`
+    fetch(url)
+    .then((response) => response.json())
+    .then((resData) => {
+        categoryItems(resData.plants)
+    })
+}
+const categoryItems = (plants) => {
+    const getPlantsContainer = document.getElementById('tree-card-container');
+        getPlantsContainer.innerHTML = '';
+        for(const plant of plants) {
+        // Get the display trees section
+
+        const createNewPlatsDiv = document.createElement('div');
+        createNewPlatsDiv.innerHTML = `
+                <div class="tree-card max-w-80 h-full p-4 bg-white rounded-xl flex flex-col justify-between">
+                    <img src="${plant.image}" alt="Picture" class="h-50 w-[100vw] rounded-xl">
+                    <h2 class="text-lg font-semibold mt-3">${plant.name}</h2>
+                    <p class="text-sm font-normal text-gray-700 text-justify mt-2">${plant.description}</p>
+                    <div class="flex justify-between items-center">
+                        <p class="px-4 py-1 rounded-2xl text-[#15803D] bg-green-200 mt-2">${plant.category}</p>
+                        <p class="font-semibold">৳${plant.price}</p>
+                    </div>
+                    <button class="w-full bg-[#15803D] p-2 rounded-3xl text-white mt-3 cursor-pointer">Add to Cart</button>
+                </div>
+        `;
+        getPlantsContainer.appendChild(createNewPlatsDiv)
+    }   
+
+}
+
 getAllPlant()
 getPlantCategory()
+
+
